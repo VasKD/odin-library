91fb0b3a57b31af38224c57d5e08cb519505cba4
VasKD <kiara.denisse96@gmail.com>
2025-03-18 19:49:15 -0400

    Add functionality for "Add a Book" button

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const modal = document.getElementById("newBookModal");
 const createBtn = document.querySelector(".addBook");
 const modalExit = document.getElementById("exit");
 const submitBook = document.getElementById("submitBook");
+const cardsContainer = document.getElementById("cards");
 
 
 
@@ -23,12 +24,12 @@ function Books(id, title, author, pageNum, cover, description, read){
     this.id = id;
     this.title = title;
     this.author = author;
-    this.pageNum = pageNum + " pages";
+    this.pageNum = pageNum;
     this.cover = cover;
     this.description = description;
     this.read = read;
     this.info = function () {
-        return `${this.title} by ${this.author}, ${this.pageNum}, ${this.read}`;
+        return `${this.title} by ${this.author}, ${this.pageNum} pages, ${this.read}`;
     }
 }
 
@@ -47,11 +48,82 @@ function addBookToLibrary(title, author, pageNum, cover, description, read) {
 
     const newBook = new Books(objectID, title, author, pageNum, cover, description, read);
     myLibrary.push(newBook);
+    addCard(newBook);
+    
     console.log(myLibrary);
     console.log(myLibrary[myLibrary.length - 1].info());
     displayBooks();
     console.log("A new book has been added to your library! " + newBook.title + " by " + newBook.author);
 
+}
+
+
+// add card to grid
+function addCard(bookObject) {
+    // create card container
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    // create image element for cover
+    const cover = document.createElement("img");
+    cover.src = bookObject.cover;
+    card.appendChild(cover);
+
+    // create container for content
+    const contentContainer = document.createElement("div");
+    contentContainer.classList.add("content");
+
+    // add title of book
+    const title = document.createElement("p");
+    title.classList.add("title");
+    title.textContent = bookObject.title;
+    console.log(title.textContent);
+    contentContainer.appendChild(title);
+
+    // add author of book
+    const author = document.createElement("p");
+    author.classList.add("author");
+    author.textContent = bookObject.author;
+    console.log(author.textContent);
+    contentContainer.appendChild(author);
+
+    // add divider line
+    const divider = document.createElement("hr");
+    console.log(divider);
+    contentContainer.appendChild(divider);
+
+    // add description of book
+    const description = document.createElement("p");
+    description.classList.add("author");
+    description.textContent = bookObject.description;
+    console.log(description.textContent);
+    contentContainer.appendChild(description);
+
+    // add bottom portion of card
+    const bottom = document.createElement("div");
+    bottom.classList.add("bottom");
+    contentContainer.appendChild(bottom);
+
+    // add page number
+    const pageNum = document.createElement("p");
+    pageNum.classList.add("pageNum");
+    pageNum.textContent = "# " + bookObject.pageNum;
+    bottom.appendChild(pageNum);
+
+    // add read button
+    const read = document.createElement("button");
+    read.classList.add("read");
+    if (bookObject.read) {
+        read.textContent = "Read";
+    } else {
+        read.textContent = "Not Read"
+    }
+    bottom.appendChild(read);
+
+
+    // add card to the DOM
+    card.appendChild(contentContainer);
+    cardsContainer.appendChild(card);
 }
 
 // validate form values
@@ -145,5 +217,6 @@ bookEntry.addEventListener("submit", (e) => {
         author = validated[1];
         addBookToLibrary(title, author, pageNum, cover, description, read);
     }
+    bookEntry.reset(); // clear form after submission
 });
 
